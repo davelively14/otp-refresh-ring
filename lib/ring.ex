9ee4a7bf7ff59af5ff7d50f3767e6f7db2ef3df3
Dave Lively <davelively14@gmail.com>
2018-01-26 09:59:25 -0500

    Added link_processes/1 and link_processes/2

--- lib/ring.ex
+++ lib/ring.ex
@@ -12,4 +12,23 @@ defmodule Ring do
         raise "Crashed"
     end
   end
+
+  # Setup aggregator
+  def link_processes(procs), do: link_processes(procs, [])
+
+  # Matches only if there are at least two elements in a list
+  def link_processes([proc_1, proc_2 | rest], linked_processes) do
+    send(proc_1, {:link, proc_2})
+    link_processes([proc_2 | rest], [proc_1|linked_processes])
+  end
+
+  # Matches when only a single element in the list and links the first proc to
+  # the the last proc in order to create the ring. Interesting to note that
+  # there are no matching functions if a user were to pass an empty list for
+  # procs.
+  def link_processes([proc | []], linked_processes) do
+    first_process = linked_processes |> List.last
+    send(proc, {:link, first_process})
+    :ok
+  end
 end
