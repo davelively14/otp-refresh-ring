5728bdc3f1de6a0bcce77e25af5bdc42edfcbef3
Dave Lively <davelively14@gmail.com>
2018-01-26 09:33:12 -0500

    Init commit

---
+++ lib/ring.ex
@@ -0,0 +1,15 @@
+defmodule Ring do
+  def create_processes(n) do
+    1..n |> Enum.map(fn _ -> spawn(fn -> loop() end) end)
+  end
+
+  def loop do
+    receive do
+      {:link, link_to} when is_pid(link_to) ->
+        Process.link(link_to)
+        loop()
+      :crash ->
+        raise "Crashed"
+    end
+  end
+end
